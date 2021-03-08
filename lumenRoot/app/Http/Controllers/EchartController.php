8d2eaeb4ad8806933a06790bf59ce627c1316bcd
谭健 <tann1013@hotmail.com>
2021-03-08 13:39:19 +0800

    feat#echart

--- lumenRoot/app/Http/Controllers/EchartController.php
+++ lumenRoot/app/Http/Controllers/EchartController.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * 说明：
+ * 1 主要图形：
+ * 1.1 条形图（单个或多个）
+ * 1.2 折线（单个或多个）
+ *
  *
  * Created by PhpStorm.
  * @author tann1013@hotmail.com
@@ -10,11 +15,14 @@
 namespace App\Http\Controllers;
 
 
+
 class EchartController extends Controller
 {
     public function module(){
         //$echartOption = $this->_optionOne();//echartOption
-        $echartOption = $this->_pipe();//echartOption
+        //$echartOption = $this->_pipe();//echartOption
+        $echartOption = $this->_optionSingleLine();
+
         return view('moduleIndex', ['echartOption' => $echartOption]);
     }
 
@@ -159,4 +167,90 @@ eof;
 
     }
 
+    private function _optionSingleLine(){
+        $html = <<<eof
+        option = {
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+    }]
+};
+eof;
+        return $html;
+    }
+
+    private function _optionMultiLine(){
+        $html = <<<eof
+        option = {
+    title: {
+        text: '折线图堆叠'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+    ]
+};
+eof;
+        return $html;
+    }
 }
