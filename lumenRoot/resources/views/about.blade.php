de088d150b55ef3c22986fc054b4a78248137715
谭健 <tann1013@hotmail.com>
2021-03-12 17:31:15 +0800

    feat#博客详情

--- lumenRoot/resources/views/about.blade.php
+++ lumenRoot/resources/views/about.blade.php
@@ -82,41 +82,20 @@
     <div class="row top-title">
         <div class="col-md-12">
             <h1 class="page-title">
-                关于Me
+                关于蜗牛工作室
             </h1>
         </div>
     </div>
     <div class="row">
         <div class="col-md-12 post-container">
-            <h2 class="post-title" style="display: none;">
-                <a href="about.php" rel="bookmark">
-                    关于Me
-                </a>
-            </h2>
             <div class="post-content">
-                <div class="row" style="display: none">
-                    <div class="col-md-12">
-                        <div class="thumbnail" style="display:block;">
-                            <a href="javascript:;">
-                                <img title="关于我 @Ybsat" src="assets/img/about.png" alt="关于我 @Ybsat"/>
-                            </a>
-                            <div class="caption">
-                                <h3>
-                                    个人概况
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="panel panel-default" id="simple-intro">
-                    <div class="panel-body">
+                <div class="panel panel-default">
+                    <div class="panel-body clearfix">
                         <h3>
-                            作者简介
+                            蜗牛工作室和作者简介
                         </h3>
-                        <p>暂无</p>
-                        <p>
-                            我的联系方式：
-                        </p>
+                        <div>5年开发经验，主要语言PHP，Go和Python，有高并发处理经验。前新浪网软件工程师，目前主要做项目管理，目前定居杭州。</div>
+                        <div>工作室主营业务，APP开发，微信小程序、微信公号和各类软件管理平台的等开发服务。</div>
                         <blockquote class="contact-list" id="about-list">
                             <ul>
                                 <li>
@@ -131,22 +110,137 @@
                                         https://github.com/tann1013
                                     </kbd>
                                 </li>
+                                <li>
+                                    我的微信:
+                                    <kbd>
+                                        identical1013
+                                    </kbd>
+                                </li>
                             </ul>
                         </blockquote>
-                    </div>
-                </div>
-                <div class="panel panel-default">
-                    <div class="panel-body clearfix">
-                        <h3>
-                            蜗牛工作室简介
-                        </h3>
-                        <p>
-                           主营业务，APP开发，微信小程序、微信公号和各类软件管理平台的等开发服务。
-                        </p>
                         <footer class="pull-right">
                             二零二零年
                             {{--<cite title="杭州">杭州</cite>--}}
                         </footer>
+                    </div>
+                </div>
+
+                <div class="col-md-12 post-container">
+                    <div class="post-content">
+                        <div class="ds-thread" data-thread-key="750" data-author-key="1" data-title="给我留言" data-url="contact.php">
+                        </div>
+                        <div id="ds-ssr">
+                            <ol id="commentlist">
+                                <li class="comment even thread-even depth-1" id="li-comment-11325">
+                                    <article id="comment-11325" class="comment">
+                                        <footer class="comment-meta">
+                                            <cite class="comment-author vcard">
+                      <span class="fn">
+                        <a href="" rel="external nofollow" class="url">
+                            异步
+                        </a>
+                      </span>
+                                                on
+                                                <a rel="nofollow" href="">
+                                                    <time pubdate="" datetime="2015-01-06T00:15:56+00:00">
+                                                        2015 年 1 月 6 日 at am 12:15
+                                                    </time>
+                                                </a>
+                                                <span class="says">
+                        said:
+                      </span>
+                                            </cite>
+                                        </footer>
+                                        <div class="comment-content">
+                                            <p>
+                                                嗨~
+                                            </p>
+                                        </div>
+                                    </article>
+                                </li>
+                                <li class="comment odd alt thread-odd thread-alt depth-1" id="li-comment-11323">
+                                    <article id="comment-11323" class="comment">
+                                        <footer class="comment-meta">
+                                            <cite class="comment-author vcard">
+                      <span class="fn">
+                        <a href="" rel="external nofollow" class="url">
+                            异步
+                        </a>
+                      </span>
+                                                on
+                                                <a rel="nofollow" href="">
+                                                    <time pubdate="" datetime="2015-01-05T17:12:39+00:00">
+                                                        2015 年 1 月 5 日 at pm 5:12
+                                                    </time>
+                                                </a>
+                                                <span class="says">
+                        said:
+                      </span>
+                                            </cite>
+                                        </footer>
+                                        <div class="comment-content">
+                                            <p>
+                                                新年快乐~
+                                            </p>
+                                        </div>
+                                    </article>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12 post-container">
+                    <div class="post-content">
+                        <form method="post" action="black/lib/contact.php" id="form1" class="contact-form">
+                            <div>
+          <span>
+            <label>
+                姓名*
+            </label>
+          </span>
+                                <span>
+            <input name="contact_name" type="text" class="textbox">
+          </span>
+                            </div>
+                            <div>
+          <span>
+            <label>
+                标题*
+            </label>
+          </span>
+                                <span>
+            <input name="contact_title" type="text" class="textbox">
+          </span>
+                            </div>
+                            <div>
+          <span>
+            <label>
+                邮箱*
+            </label>
+          </span>
+                                <span>
+            <input name="contact_email" type="email" class="textbox">
+          </span>
+                            </div>
+                            <div>
+          <span>
+            <label>
+                内容*
+            </label>
+          </span>
+                                <span>
+            <textarea name="contact_con" class="textbox">            </textarea>
+          </span>
+                            </div>
+                            <div>
+          <span>
+            <input type="submit" value="提交" name="tijiao">
+          </span>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="meta-box">
                     </div>
                 </div>
             </div>
