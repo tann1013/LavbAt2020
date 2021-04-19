5da9b38d97e84bde56fef9c4c7672bd39d9d8485
谭健 <tann1013@hotmail.com>
2021-04-19 09:55:25 +0800

    feat#新增extShells

--- lm/extShells/json.sh
+++ lm/extShells/json.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #>>>>>>>>>>每五分钟生成json文件，如pointlist_20210309140554110.json
+#设置路径配置
+#线上
+#BASE_PATH=/opt/lampp/LumenAdminVueBasicAt2020
+#本地
+BASE_PATH=/Users/tanjian/web/woniuStudioProjects/LavbAt2020
+
+
 #日期，使用方式Eg.huadian_bak_${NOW_DATE}
 NOW_DATE="`date +%Y%m%d%H%M`"
 NEW_FILE="pointlist_${NOW_DATE}.json"
-cp /opt/lampp/LumenAdminVueBasicAt2020/exttmp/tpl/pointlist_v419.json /opt/lampp/LumenAdminVueBasicAt2020/exttmp/${NEW_FILE}
+cp ${BASE_PATH}/exttmp/tpl/pointlist_v419.json ${BASE_PATH}/exttmp/${NEW_FILE}
 echo "已生成文件，${NEW_FILE}！"
