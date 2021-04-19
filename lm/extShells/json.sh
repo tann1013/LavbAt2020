e16457b555e0486833782b7057a6a5f13383d9e0
谭健 <tann1013@hotmail.com>
2021-04-19 09:50:49 +0800

    feat#新增extShells

--- lm/extShells/json.sh
+++ lm/extShells/json.sh
@@ -3,5 +3,5 @@
 #日期，使用方式Eg.huadian_bak_${NOW_DATE}
 NOW_DATE="`date +%Y%m%d%H%M`"
 NEW_FILE="pointlist_${NOW_DATE}.json"
-cp ../../exttmp/tpl/pointlist_v419.json ../../exttmp/${NEW_FILE}
+cp /opt/lampp/LumenAdminVueBasicAt2020/exttmp/tpl/pointlist_v419.json /opt/lampp/LumenAdminVueBasicAt2020/exttmp/${NEW_FILE}
 echo "已生成文件，${NEW_FILE}！"
