4fbeb9c6298c7e5e5689df164349a3153cc8e4a8
谭健 <tann1013@hotmail.com>
2021-06-01 14:53:16 +0800

    feat#搭建soap服务

--- lm/public/wbs.php
+++ lm/public/wbs.php
@@ -5,4 +5,15 @@
  * @date 6/1/21
  * @version 1.0
  */
-echo '2';
+
+class SERVER{
+    public function HelloWorld(){
+        return "Hello World!" . __FILE__;
+    }
+}
+
+
+
+$objSoapServer = new SoapServer("SERVER.wsdl");
+$objSoapServer->setClass("SERVER");//注册类,SERVER
+$objSoapServer->handle();
