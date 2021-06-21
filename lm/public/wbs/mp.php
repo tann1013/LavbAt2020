7d9b6a99886319c3c0589d4f6baae6458f4aa4e1
谭健 <tann1013@hotmail.com>
2021-06-21 21:22:25 +0800

    feat#模拟南自服务

--- lm/public/wbs/mp.php
+++ lm/public/wbs/mp.php
@@ -3,7 +3,7 @@
  * 模拟南自服务端
  */
 
-const KUNSHI_REC_URL = 'http://hd2.com/wbs/SERVER.php';
+const KUNSHI_REC_URL = 'http://hd2.com/wbs/SERVER.php?wsdl';
 
 function _writeLog($content){
 //    $path =  __DIR__ . '/wbs-'.date('Y-m-d').'.log';
@@ -14,12 +14,12 @@ function _writeLog($content){
 /**
  * @param $reqId
  */
-function _sendJsonData2ks($reqId = 0){
+function _sendJsonData2ks(){
 
     /**
      * 1 配置
      */
-    $withXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><ns1:dataReq soapenv:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:ns1=\"http://tempuri.org/\"><ns1:arg0 xsi:type=\"soapenc:string\" xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\"><?xml version=\"1.0\" encoding=\"UTF-8\"?><replyDataMsg><replyId>$reqId</replyId><facName>hd</facName><replyFlag>1</replyFlag><replyData>[{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-日发电量\",\"num\":\"1\",\"value\":\"3628.42\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-累计发电量\",\"num\":\"2\",\"value\":\"7.15178e+06\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-总SO2减排\",\"num\":\"3\",\"value\":\"214553\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-总NOx减排\",\"num\":\"4\",\"value\":\"107277\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-总减排标煤量\",\"num\":\"5\",\"value\":\"2.86071e+06\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-输出总有功功率\",\"num\":\"6\",\"value\":\"0\"}]</replyData></replyDataMsg></ns1:arg0></ns1:dataReq></soapenv:Body></soapenv:Envelope>";
+    $withXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><ns1:dataReq soapenv:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:ns1=\"http://tempuri.org/\"><ns1:arg0 xsi:type=\"soapenc:string\" xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\"><?xml version=\"1.0\" encoding=\"UTF-8\"?><replyDataMsg><replyId>9999</replyId><facName>hd</facName><replyFlag>1</replyFlag><replyData>[{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-日发电量\",\"num\":\"1\",\"value\":\"3628.42\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-累计发电量\",\"num\":\"2\",\"value\":\"7.15178e+06\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-总SO2减排\",\"num\":\"3\",\"value\":\"214553\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-总NOx减排\",\"num\":\"4\",\"value\":\"107277\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-总减排标煤量\",\"num\":\"5\",\"value\":\"2.86071e+06\"},{\"datasource\":\"上海创智园光伏站\",\"name\":\"光伏区-cpu-输出总有功功率\",\"num\":\"6\",\"value\":\"0\"}]</replyData></replyDataMsg></ns1:arg0></ns1:dataReq></soapenv:Body></soapenv:Envelope>";
     /**
      * 2 实例化
      */
@@ -30,7 +30,7 @@ function _sendJsonData2ks($reqId = 0){
     $response = $soap->__doRequest($withXml, $wsdlUrl, 'getAlogpsResults',1,0);//发送xml必须使用__doRequest
 
     //3 判断是否成功
-    var_dump($response);die;
+    //var_dump($response);die;
 }
 
 /**
@@ -70,44 +70,41 @@ function _filterSpecialChar($jsonStr){
 
 try{
 
-
-
-    //header("Content-Type: text/html;charset=utf-8");
-    _writeLog('开始处理坤时的请求...');
-
-    _writeLog('--坤时请求withXml数据--');
-    //_writeLog(var_export($_SERVER, true));
-    $ks_xmlstrem = file_get_contents("php://input");
-    _writeLog($ks_xmlstrem);
-
-
-    /**
-     * 解析得到$replyId
-     */
-    $ks_xmlstrem = _filterSpecialChar($ks_xmlstrem);
-    $reqId = _stepGetReqId($ks_xmlstrem);
-    _writeLog('$reqId='.$reqId);
-
-    /**
-     * 3 给坤时返回数据
-     */
-    //_sendJsonData2ks();
-
-
-
-    
-
-    //1 定义类
-    class SERVER{
-        public function HelloWorld(){
-            return "Hello World!" . __FILE__;
+    if($_SERVER['REQUEST_METHOD']=='GET'){
+        class SERVER{
+            public function HelloWorld(){
+                return "Hello World!" . __FILE__;
+            }
         }
+        $objSoapServer = new SoapServer("SERVER.wsdl");
+        $objSoapServer->setClass("SERVER");//注册类,SERVER
+        $objSoapServer->handle();
+
+    }else{
+
+        //header("Content-Type: text/html;charset=utf-8");
+        _writeLog('开始处理坤时的请求...');
+
+        _writeLog('--坤时请求withXml数据--');
+        //_writeLog(var_export($_SERVER, true));
+        $ks_xmlstrem = file_get_contents("php://input");
+        _writeLog($ks_xmlstrem);
+
+
+        /**
+         * 解析得到$replyId
+         */
+        $ks_xmlstrem = _filterSpecialChar($ks_xmlstrem);
+        $reqId = _stepGetReqId($ks_xmlstrem);
+        _writeLog('$reqId='.$reqId);
+
+        /**
+         * 3 给坤时返回数据
+         */
+        _sendJsonData2ks();
+
+
     }
-
-    $objSoapServer = new SoapServer("SERVER.wsdl");
-    $objSoapServer->setClass("SERVER");//注册类,SERVER
-    $objSoapServer->handle();
-
 }catch (Exception $e){
     _writeLog($e->getMessage());
 }
