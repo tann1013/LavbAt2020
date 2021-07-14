99a05daac0ee6bebb4bb8fe47301fff7a9f6e978
谭健 <tann1013@hotmail.com>
2021-07-14 18:32:29 +0800

    feat#账单新增导入

--- lm/app/Http/Controllers/Admin/BillController.php
+++ lm/app/Http/Controllers/Admin/BillController.php
@@ -77,6 +77,26 @@ class BillController extends CController
         }
     }
 
+    /**
+     * 计算漏记天数
+     * @param $thisMonthArr
+     * @param $keyM
+     * @return int
+     */
+    private function _getMissingDay($thisMonthArr, $keyM){
+        //本月天数
+        $start_day = date('Ym01', strtotime($keyM));
+        $end_day   = date('Ymd', strtotime("{$start_day} + 1 month -1 day"));
+        $thisMonthRange =  range($start_day, $end_day, 1);
+        $thisMonthDays = sizeof($thisMonthRange);
+
+        //已记录天数
+        $jlDays = sizeof($thisMonthArr);
+        $num = $thisMonthDays - $jlDays;
+        return $num;
+    }
+
+
     private function _formatGetList($result){
         $newRows = [];
         if($rows = $result['rows']){
@@ -86,13 +106,16 @@ class BillController extends CController
                 //增加+-符号
                 $this->_addStrForItem($item);
 
+                //漏记天数
+                $ljNum = $this->_getMissingDay($item, $keyM);
+
                 //1 monthData
                 $thisMonthData = [
                     "id" => time(),
                     "op_eat" => '',
                     "op_traffic" => '',
                     "op_other" => '',
-                    "op_other_notes" => '记录为'.$keyM.'（已漏记10天）。',
+                    "op_other_notes" => '记录为'.$keyM.'（已漏记'.$ljNum.'天）。',
                     "total" => '',
                     "op_today_profit" => '',
                     "op_today_profit_notes" => '',
